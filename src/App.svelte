043d3ff87b6890853fb5059d7c567edf12601c73
Austin Merrick <austin.merrick@unlv.edu>
2021-10-28 19:43:27 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+	import DiscordMessage from './DiscordMessage.svelte'
+
+	let messages = [];
+
+
+	onMount(async () => {
+		const res = await fetch(`https://discord-broadcast.herokuapp.com/`);
+		messages = await res.json();
+	});
+</script>
+
+<main>
+	<h1>discord broadcast 📣</h1>
+	{#each messages as message}
+		<DiscordMessage {...message}/>
+	{/each}
+</main>
+
+<style>
+	h1 {
+		font-color: var(--pure-white);
+	}
+	main {
+		margin: auto;
+		max-width: 40em;
+	}
+</style>
---
+++ src/DiscordMessage.svelte
@@ -0,0 +1,50 @@
+<script>
+	export let content;
+	export let createdTimestamp;
+	export let displayName;
+	export let avatarURL;
+	export let attachmentURLs
+</script>
+
+<div class="message">
+	<img class="avatar" src="{avatarURL}" alt="profile picture for {displayName}">
+	<div>
+		<span class="name">{displayName}</span>
+		<span class="date">{new Date(createdTimestamp).toLocaleString()}</span>
+		<p>{content}</p>
+		{#each attachmentURLs as attachmentURL}
+			<img src="{attachmentURL}" alt="attachment">
+		{/each}
+	</div>
+</div>
+
+<style>
+	.avatar {
+		border-radius: 100%;
+		height: 3rem;
+		margin-right: .5rem;
+	}
+	.name {
+		color: var(--pure-white);
+	}
+	.date {
+		margin-left: .25rem;
+		color: var(--text-muted);
+		font-size: .75rem
+	}
+	.message {
+		display: flex;
+		margin: .5rem;
+		padding: .5rem;
+		color: var(--text-normal);
+	}
+	p {
+		margin: .5rem 0;
+	}
+	div {
+		border-radius: 1em;
+	}
+	img {
+		max-width: 100%;
+	}
+</style>
